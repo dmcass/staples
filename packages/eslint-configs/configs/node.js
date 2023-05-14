0b5cc55640dc3ee65e95710a7e1cda21d7d634e3
Dustin Cass <dustin.cass@gmail.com>
2023-05-14 00:55:10 -0700

    Add node rules

---
+++ packages/eslint-configs/configs/node.js
@@ -0,0 +1,47 @@
+module.exports = {
+    plugins: ['n'],
+    rules: {
+        // Best Practices
+        'n/no-deprecated-api': 'error',
+
+        // Possible Errors
+        'n/handle-callback-err': ['error', 'error'],
+        'n/no-callback-literal': 'error',
+        'n/no-exports-assign': 'error',
+        'n/no-extraneous-import': 'error',
+        'n/no-extraneous-require': 'error',
+        'n/no-missing-import': 'error',
+        'n/no-missing-require': 'error',
+        'n/no-new-require': 'error',
+        'n/no-path-concat': 'error',
+        'n/no-process-exit': 'error',
+        'n/no-unpublished-bin': 'error',
+        'n/no-unpublished-import': 'error',
+        'n/no-unpublished-require': 'error',
+        'n/no-unsupported-features/es-builtins': 'error',
+        'n/no-unsupported-features/es-syntax': 'error',
+        'n/no-unsupported-features/node-builtins': 'error',
+        'n/process-exit-as-throw': 'error',
+        'n/shebang': 'error',
+
+        // Stylistic Issues
+        'n/callback-return': 'off',
+        'n/exports-style': ['error', 'module.exports', { allowBatchAssign: false }],
+        'n/file-extension-in-import': ['error', 'always'],
+        'n/global-require': 'error',
+        'n/no-mixed-requires': 'error',
+        'n/no-process-env': 'error',
+        'n/no-restricted-import': 'off',
+        'n/no-restricted-require': 'off',
+        'n/no-sync': 'off',
+        'n/prefer-global/buffer': 'error',
+        'n/prefer-global/console': 'error',
+        'n/prefer-global/process': 'error',
+        'n/prefer-global/text-decoder': 'error',
+        'n/prefer-global/text-encoder': 'error',
+        'n/prefer-global/url-search-params': 'error',
+        'n/prefer-global/url': 'error',
+        'n/prefer-promises/dns': 'error',
+        'n/prefer-promises/fs': 'error',
+    },
+};
