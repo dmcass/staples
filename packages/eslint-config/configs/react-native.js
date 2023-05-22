85d81daf1cbab434c81c5bfb106ee790a861870f
Dustin Cass <dustin.cass@gmail.com>
2023-05-22 00:52:27 -0700

    Fix react-native rules

--- packages/eslint-config/configs/react-native.js
+++ packages/eslint-config/configs/react-native.js
@@ -1,16 +1,16 @@
 module.exports = {
     plugins: ['react-native'],
     rules: {
-        'no-color-literals': 'error',
-        'no-inline-styles': 'error',
+        'react-native/no-color-literals': 'error',
+        'react-native/no-inline-styles': 'error',
 
         // Causes eslint to crash in VSCode
         // https://github.com/Intellicode/eslint-plugin-react-native/issues/270
-        'no-raw-text': 'off',
+        'react-native/no-raw-text': 'off',
 
-        'no-single-element-style-arrays': 'error',
-        'no-unused-styles': 'error',
-        'sort-styles': ['error', 'asc', { ignoreStyleProperties: true }],
-        'split-platform-components': 'error',
+        'react-native/no-single-element-style-arrays': 'error',
+        'react-native/no-unused-styles': 'error',
+        'react-native/sort-styles': ['error', 'asc', { ignoreStyleProperties: true }],
+        'react-native/split-platform-components': 'error',
     },
 };
